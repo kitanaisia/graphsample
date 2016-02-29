9873f7b4263364187b28ca6df78804e20203f557
Kensuke Hara <hara.kensuke@info.seino.co.jp>
2016-02-29 05:17:37 +0000

    Draw chart using database

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -5,36 +5,31 @@ class GraphsController < ApplicationController
   # GET /graphs.json
   def index
     @graphs = Graph.all
+    p Graph.pluck("date")
+    p Graph.pluck("temp")
 
     @data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: Graph.pluck("date"),
       datasets: [
         {
           label: "My First dataset",
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "# fff",
-          pointHighlightFill: "# fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-
-        },
-        {
-          label: "My Second dataset",
           fillColor: "rgba(151,187,205,0.2)",
           strokeColor: "rgba(151,187,205,1)",
           pointColor: "rgba(151,187,205,1)",
           pointStrokeColor: "# fff",
           pointHighlightFill: "# fff",
           pointHighlightStroke: "rgba(151,187,205,1)",
-          data: [28, 48, 40, 19, 86, 27, 90]
-
-        }
+          data: Graph.pluck("temp") 
+        },
       ]
 
     }
-    @options = {}
+    @options = {
+      scaleOverride: true,
+      scaleStartValue: 0,
+      scaleSteps: 6,
+      scaleStepWidth: 5
+    }
   end
 
   # GET /graphs/1
